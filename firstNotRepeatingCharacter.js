b7bd72eaaf40715ba3ce687c6b7ab13b3c9b1015
kherin <kherin.j.s.bundhoo@gmail.com>
2019-08-17 22:07:50 +0400

    removed unused variable

--- firstNotRepeatingCharacter.js
+++ firstNotRepeatingCharacter.js
@@ -3,7 +3,6 @@
 */
 
 function firstNotRepeatingCharacter(s) {
-    let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m", "n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     let sorted = s.split('').sort().join('');
     for(let val of s) {
       if(sorted.indexOf(val + val) == -1 && sorted.indexOf(val) > -1) return val;
