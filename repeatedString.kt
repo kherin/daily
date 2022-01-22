731dcdb7a4fb8da34030173154bcb2a6a8e961e8
Kherin <kherin.j.s.bundhoo@gmail.com>
2022-01-22 19:57:23 +0400

    wip: added repeating string function

--- repeatedString.kt
+++ repeatedString.kt
@@ -3,9 +3,17 @@ fun main() {
     val s: String = "abcac"
     val n: Int = 10
 
+    println("Result is ${repeatedString(s, n)}")
 }
 
-fun repeatedString(s: String, n: Int) {
-    val characters: List<String> = s.split("")
-    
+fun repeatedString(s: String, n: Int): Int {
+    val characters: List<String> = s.split("").filter { it.isNotBlank() }
+    val baseStringSize: Int = characters.size
+
+    val repeatFactor: Int = n - (n % baseStringSize)
+
+    val repeatingList: List<String> = List(repeatFactor) { s }
+    repeatingList.plus(s.substring(0, baseStringSize - 1))
+
+    return repeatingList.count { it == "a" }
 }
