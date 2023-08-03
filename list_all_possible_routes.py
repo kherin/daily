f50f0858d84be763b9e733bf1981e673977685eb
Kherin <kherin.j.s.bundhoo@gmail.com>
2023-08-03 07:35:41 +0400

    numpy: adding notebook

---
+++ list_all_possible_routes.py
@@ -0,0 +1,12 @@
+port_names = ["PAN", "AMS", "CAS", "NYC", "HEL"]
+
+
+def permutations(route, ports):
+    # Write your recursive code here
+
+    # Print the port names in route when the recursion terminates
+    print(' '.join([port_names[i] for i in route]))
+
+
+# This will start the recursion with 0 ("PAN") as the first stop
+permutations([0], list(range(1, len(port_names))))
