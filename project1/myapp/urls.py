3b886bedbcb160478468a4c7bd36fccd86c67fc4
Kherin <kherin.j.s.bundhoo@gmail.com>
2023-12-11 22:07:55 +0400

    learn: path params

--- project1/myapp/urls.py
+++ project1/myapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('hello/<name>/<id>', views.pathview, name='pathview'),
+    path('showform/', views.showform, name='showform'),
 ]
--- project1/myapp/views.py
+++ project1/myapp/views.py
@@ -4,3 +4,11 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Welcome to Little Lemon restaurant!")
+
+
+def pathview(request, name, id):
+    return HttpResponse(f"Hello {name}, your id is {id}")
+
+
+def showform(request):
+    return render(request, "form.html")
