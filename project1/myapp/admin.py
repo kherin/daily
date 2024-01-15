169fc87b04bf5e39a856b77f367a70335773d28e
Kherin <kherin.j.s.bundhoo@gmail.com>
2024-01-15 23:41:35 +0400

    migrations: added models for Person and Drink

--- project1/myapp/admin.py
+++ project1/myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from myapp.models import Person, Drinks
+
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Drinks)
--- project1/myapp/models.py
+++ project1/myapp/models.py
@@ -5,3 +5,8 @@ class Person(models.Model):
     name = models.CharField(max_length=20)
     email = models.EmailField()
     phone = models.CharField(max_length=20)
+
+
+class Drinks(models.Model):
+    drink_name = models.CharField(max_length=200)
+    price = models.IntegerField()
