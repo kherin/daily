fb9e924ba5ace92cefa3003f948519a14bd54ff6
Kherin <kherin.j.s.bundhoo@gmail.com>
2024-01-16 13:37:00 +0400

    models: added DrinksCategory model

--- project1/myapp/admin.py
+++ project1/myapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from myapp.models import Person, Drinks
+from myapp.models import Person, Drinks, DrinksCategory
 
 # Register your models here.
 admin.site.register(Person)
 admin.site.register(Drinks)
+admin.site.register(DrinksCategory)
--- project1/myapp/models.py
+++ project1/myapp/models.py
@@ -7,6 +7,12 @@ class Person(models.Model):
     phone = models.CharField(max_length=20)
 
 
+class DrinksCategory(models.Model):
+    category_name = models.CharField(max_length=200)
+
+
 class Drinks(models.Model):
     drink_name = models.CharField(max_length=200)
     price = models.IntegerField()
+    category_id = models.ForeignKey(
+        DrinksCategory, on_delete=models.PROTECT, default=None)
