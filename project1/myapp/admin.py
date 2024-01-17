e87fbc6fb689d3eed48a8d2c65bcc298c6adab1f
Kherin <kherin.j.s.bundhoo@gmail.com>
2024-01-17 22:41:39 +0400

    models: added Vehicle, Person models

--- project1/myapp/admin.py
+++ project1/myapp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from myapp.models import Person, Drinks, DrinksCategory
+from myapp.models import Person, Drinks, DrinksCategory, Customer, Vehicle
 
 # Register your models here.
 admin.site.register(Person)
 admin.site.register(Drinks)
 admin.site.register(DrinksCategory)
+admin.site.register(Customer)
+admin.site.register(Vehicle)
--- project1/myapp/models.py
+++ project1/myapp/models.py
@@ -16,3 +16,13 @@ class Drinks(models.Model):
     price = models.IntegerField()
     category_id = models.ForeignKey(
         DrinksCategory, on_delete=models.PROTECT, default=None)
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Vehicle(models.Model):
+    name = models.CharField(max_length=255)
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name='Vehicle')
