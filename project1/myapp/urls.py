f038922bb29ea8eaa201876b0cb618cb0d943c29
Kherin <kherin.j.s.bundhoo@gmail.com>
2024-01-26 20:30:20 +0400

    learning about class-based views

--- project1/myapp/urls.py
+++ project1/myapp/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views
 
+from myapp.views.index import IndexView
+
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('hello/<name>/<id>', views.pathview, name='pathview'),
-    path('showform/', views.showform, name='showform'),
-    path('getform/', views.getform, name='getform'),
-    path("drinks/<str:drink_name>", views.drinks, name="drink_name"),
-    path('menu', views.menu, name='menu'),
-    path('about', views.about, name='about'),
+    # path('', views.home, name='home'),
+    # path('hello/<name>/<id>', views.pathview, name='pathview'),
+    # path('showform/', views.showform, name='showform'),
+    # path('getform/', views.getform, name='getform'),
+    # path("drinks/<str:drink_name>", views.drinks, name="drink_name"),
+    # path('menu', views.menu, name='menu'),
+    # path('about', views.about, name='about'),
+    path('index', IndexView.as_view(), name='index'),
 ]
